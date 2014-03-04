b69b102e1601125d5d92b1a64153a8981ca3e533
Scott Daner <scottdaner@r4-128-61-95-77.res.gatech.edu>
2014-03-04 07:15:19 -0500

    Sanitizing and Password Hashing
    Added code to sanitize input properly, and hash passwords.
    Still need to fix scripts on login.php.
    I think the reason it isn't working is because I need to separate it and use Slim and Smarty to generate the actual javascript.
    Will figure this out later today.

--- api/users.php
+++ api/users.php
@@ -1,11 +1,10 @@
 <?php 
 
 require_once 'api/prefix.php';
-$_DELETE = array();
 $app = \Slim\Slim::getInstance();
 
 
-$app->post("/user", function() {
+$app->post("/users", function() {
 	$username = $_POST['username'];
 	$password = $_POST['pass'];
 	$email = $_POST['email'];
@@ -13,13 +12,13 @@ $app->post("/user", function() {
 	createUser($username, $email, $password);
 });
 
-$app->delete("/user/", function() {
-	global $_DELETE;
+$app->delete("/users", function() {
+	// Put input into a sensible array, like how POST requests work
 	$_DELETE = array();
 	parse_str(file_get_contents('php://input'), $_DELETE);
 	
 	$username = $_DELETE['username'];
-	$password = $_PASSWORD['password'];
+	$password = $_DELETE['password'];
 
 	removeUser($username, $password);
 });
@@ -27,12 +26,12 @@ $app->delete("/user/", function() {
 function createUser($username, $email, $password) {
 	global $conn;
 
-	// TODO:
-	// Input escaping, and "sanitizing"
-	// Password hashing
-	// Possible security enhancements in the future
 
-	$query = sprintf("INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s')", $username, $email, $password);
+	// Clean up the input provided from the user before hashing and storing
+	sanitizeInput();
+
+	$query = sprintf("INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s')", 
+		$username, $email, checksum($username, $password));
 	mysqli_query($conn, $query) or die("query: ".$query." failed. " . mysql_error());
 
 	echo json_encode(array(
@@ -41,12 +40,56 @@ function createUser($username, $email, $password) {
 	));
 }
 
-function removeUser($uid) {
-	// TODO: password authentication
+function removeUser($username, $password) {
+	global $conn;
 
+	// First, need the user_id
+	$user_id = user_id($username, $password);
+	if ($user_id == -1) {
+		echo json_encode(array(
+			"result" => $user_id
+		));
+	}
+
+	// Delete that user from the database, and return the user_id to show a successful delete=
 	$query = sprintf("DELETE FROM users WHERE id == %d", $user_id);
-	mysqli_query($conn, $query) or die("query: ".$query." failed. " . mysql_error());
+	mysqli_query($conn, $query);
+	echo json_encode(array(
+		"result" => $user_id
+	));
 }
 
+function user_id($username, $password) {	
+	global $conn;
+	$query = sprintf("SELECT id FROM users WHERE name == '%s' AND password == '%s'", 
+		$username, checksum($username, $password));
+
+	$result = mysqli_query($conn, $query);
+
+	if (mysqli_num_rows($result) != 1) {
+		return -1;
+	}
+
+	$row = mysqli_fetch_assoc($result);
+
+	return $row['id'];
+}
+
+function sanitizeInput() {
+	$username = $_POST['username'];
+	$password = $_POST['pass'];
+	$email = $_POST['email'];
+
+	$s_username = filter_var($username, FILTER_SANITIZE_STRING);
+	$s_pass = filter_var($password, FILTER_SANITIZE_STRING);
+	$s_email = filter_var($email, FILTER_SANITIZE_EMAIL);
+}
+
+
+function checksum($username, $password) {
+	$c_input = $username . "____" . $password;
+	$checksum = password_hash($c_input, PASSWORD_BCRYPT);
+	return $checksum;
+}
 
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,42 @@
 <html>
 
-<form action="api/user" method="POST">
+Create an Account
+<form action="api/users" method="POST">
 Username: <input type="text" name="username"><br />
 Email: <input type="text" name="email"><br />
 Password: <input type="password" name="pass"> <br />
 <input type="submit">
 </form>
+
+
+Delete Your Account
+<form onsubmit="requestDelete()">
+Usermame <input type="text" id="delete_username" /> <br /> 
+Password: <input type="password" id="delete_password"> <br />
+<input type="submit">
+</form>
+
+
+<script>
+
+function requestDelete() {
+	var name = document.getElementById("delete_username").value;
+	var pass = document.getElementById("delete_password").value;
+	console.log("requesting delete");
+	$.ajax({
+		type: "DELETE",
+		url: "api/users",
+		data:
+		{
+			name: username,
+			password: pass
+		},
+		success: function(message) {
+			console.log(message);
+		}
+	});
+}
+
+</script>
 
 </html>

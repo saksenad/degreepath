e69d97d7aca76bcbd246ebcee4d6d4a0dd534c0d
Scott Daner <scottdaner@Scotts-MacBook-Pro-3.local>
2014-03-08 13:55:59 -0500

    Fixed undefined index bug

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $session_stat = session_status();
 
 if ($session_stat == PHP_SESSION_DISABLED || 
 	$session_stat == PHP_SESSION_NONE ||
+	!isset($_SESSION['username']) ||
+	!isset($_SESSION['user_id']) ||
 	$_SESSION['username'] == null || 
 	$_SESSION['user_id'] == null) 
 {

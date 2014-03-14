ec6eb7243ef6d1c431eb9c9e3ebf448b833a9e9f
Scott Daner <scottdaner@lawn-128-61-28-72.lawn.gatech.edu>
2014-03-14 07:52:08 -0400

    Fixes #11: Created id, and composite primary key on user_id and id.
    So, this is actually what I meant to do in the first place :)
    Sorry about the confusion.
    
    So, on one side, Naomi, you were right. It seems there has to be some unique key for every table.
    What I was trying to, and what I thought was possible, was just skip this part and just have a clustered index on the table, but MySQL doesn't do what I was thinking I guess, so that is why I got confused when reading instructions online.
    
    Anyway, now what happens is each row can be identified by an id, user_id, and the term code of the semester.
    However, when you actually look at the table at any given time, it will always be physically sorted by user_id.
    Something like this.
    user_id     id          term_code
    2      	    4           ....
    2      	    5           ....
    2           6           ...
    2           7           ..
    5           1           .
    5           2           .
    5           3	        .
    7           8	        .
    7           9	        .
    7           10	        .
    7           11	        .
    7           12	        .
    7           13	        .
    
    So that way when we make queries, they are acutally sorted pysically in space by user_id.
    Now whether this will make a HUGE difference in queries is debable, but if we want this website to scale to a lot of users, I think this is something we should do in our tables. I'm going to go through and do this to some other tables that use user_id.

--- scripts/update.sql
+++ scripts/update.sql
@@ -1,36 +1,27 @@
 
 USE degreepath;
 
-
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  first_name varchar(255),
-  last_name varchar(255),
-  major varchar(255), 
-  minor varchar(255),
-  matriculation int(10),
-  username varchar(255),
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-);
+DROP TABLE IF EXISTS user_semesters;
+DROP TABLE IF EXISTS user_subjects;
 
 DROP TABLE IF EXISTS user_semesters;
 
 CREATE TABLE user_semesters (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  id int(10) NOT NULL AUTO_INCREMENT,
   user_id int(10) NOT NULL,
   term_code int(10) NOT NULL,
-  PRIMARY KEY (id) 
+  PRIMARY KEY (user_id ASC, id ASC), 
+  CONSTRAINT pk_constraint
+  UNIQUE (id)
 );
 
 DROP TABLE IF EXISTS user_subjects;
 
 CREATE TABLE user_subjects (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  id int(10) NOT NULL AUTO_INCREMENT,
   user_id int(10) NOT NULL,
   subject varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (user_id ASC, id ASC), 
+  CONSTRAINT pk_constraint
+  UNIQUE (id)
 );

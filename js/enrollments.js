c2c80cc8b5ffa93b03c04edc264f7724925870b5
Naomi <naomi.robert@gatech.edu>
2014-04-24 00:28:20 -0400

    Fix prereq error for deleting enrollments too

--- js/enrollments.js
+++ js/enrollments.js
@@ -27,7 +27,13 @@ function deleteEnrollment(x) {
           $("[data-cid="+classArray['id']+"]").addClass('preReqPassed').removeClass('preReqFailed');
         });
         $.each(preReqChangesJSON['fail'], function(index, classArray) {
-          $("[data-cid="+classArray['id']+"]").addClass('preReqFailed').removeClass('preReqPassed');
+          // If there is more than one -- there is also one in the accordion
+          if (($("[data-cid="+classArray['id']+"]")).length > 0) {
+            $("[data-cid="+classArray['id']+"]").eq(1).addClass('preReqFailed').removeClass('preReqPassed');
+          }
+          else {
+            $("[data-cid="+classArray['id']+"]").addClass('preReqFailed').removeClass('preReqPassed');
+          }
         });
         
       /* Update semester with correct number of credit hours */
